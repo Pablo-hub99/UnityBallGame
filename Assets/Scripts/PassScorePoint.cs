bac07989a326730bd35df3bb338e3fe3ff955451
Pablo Saenz <pablosnf.99@gmail.com>
2024-03-06 15:18:39 +0100

    Proyecto Unity

---
+++ Assets/Scripts/PassScorePoint.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassScorePoint : MonoBehaviour
+{
+    //Utilizamos el método onTriggerEnter para que cuando se detecte que pasa por el Trigger añada 1 a la puntuación
+    private void OnTriggerEnter(Collider other)
+    {
+        //Añadimos 1 a la puntuación
+        GameManager.singleton.addPuntuacion(1);
+        //Añadimos 1 al número de pasos perfectos de discos
+        FindObjectOfType<BolaController>().pasosPerfectos++;
+
+    }
+}
+
